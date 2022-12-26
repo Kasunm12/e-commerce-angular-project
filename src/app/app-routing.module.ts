32bc61e02d3888066b161cae9dd066df01fc4883
Kasun M.Weerasekara <98303334+Kasunm12@users.noreply.github.com>
2022-12-27 00:15:46 +0530

    seller API Integration update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
 
 const routes: Routes = [
   {
+    component: HomeComponent,
     path: '',
-    component: HomeComponent
+    
   },
   {
+    component: SellerAuthComponent,
     path: 'seller-auth',
-    component: SellerAuthComponent
+    
+  },
+  {
+    component: SellerHomeComponent,
+    path: 'seller-home',
+    
   }
 
 ];
